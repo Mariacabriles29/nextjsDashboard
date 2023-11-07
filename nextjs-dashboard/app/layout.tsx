62a8180d9cf547f10f5c6f38f95a0ac95ec102d0
mariacabriles29 <mecp2992@gmail.com>
2023-11-07 17:11:16 -0400

    feat: :art: add fonts and upload images

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,6 @@
 /*Este archivo envuelve toda la pagina page.tsx 
 el h1 es un layout que envuelve el children->el children es el page.tsx  */
+import { montserrat } from "./ui/fonts";
 import "./ui/global.css";
 export default function RootLayout({
   children,
@@ -8,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
   );
 }
